0b80842892ea4332f510e98f0caad06e0b2d313f
jmonahan25 <jrmonahan@wisc.edu>
2024-09-28 15:28:49 -0500

    Added Sound effect

--- Camera3D.gd
+++ Camera3D.gd
@@ -1,6 +1,7 @@
 extends Camera3D
 
 @export var speed = 20
+var footsteps = load("res://heavy-footsteps-walking-35722.mp3")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,6 +13,9 @@ func _process(delta):
 	var velocity = Vector3.ZERO
 	if Input.is_action_pressed("Walk"):
 		velocity.x -= 0.01;
+		if !$AudioStreamPlayer3D.is_playing():
+			$AudioStreamPlayer3D.stream = footsteps
+			$AudioStreamPlayer3D.play()
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 	
